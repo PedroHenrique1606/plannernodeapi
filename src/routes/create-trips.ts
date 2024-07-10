fc45b634d36c9b0562d88f5b1421f9d4b326c71a
PedroHenrique1606 <pedromelo.dev.contato@gmail.com>
2024-07-09 22:09:14 -0300

    feat: implemented send emails

--- src/routes/create-trips.ts
+++ src/routes/create-trips.ts
@@ -1,8 +1,56 @@
 import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
 import {z} from 'zod'
+import dayjs from "dayjs";
+import { prisma } from "../lib/prisma";
+import nodemailer from 'nodemailer'
+import { getMailClient } from "../lib/mail";
 
 export async function createTrip(app:FastifyInstance){
-    app.post('/trips',async () => {
-        return 'hello meu fi'
+    app.withTypeProvider<ZodTypeProvider>().post('/trips',{
+        schema:{
+            body: z.object({
+                destination: z.string().min(4),
+                starts_at: z.coerce.date(),
+                ends_at: z.coerce.date(),
+                owner_name: z.string(),
+                owner_email: z.string().email(),
+            })
+        }
+    }, async (request) => {
+        const {destination,ends_at, starts_at, owner_email, owner_name} = request.body
+
+        if(dayjs(starts_at).isBefore(new Date())){
+            throw new Error('Invalid trip start date')
+        }
+
+        if(dayjs(ends_at).isBefore(starts_at)){
+            throw new Error('Invalid trip end date')
+        }
+
+        const trip  = await prisma.trip.create({
+            data: {
+                destination,
+                starts_at,
+                ends_at
+            }
+        })
+        const mail = await getMailClient()
+
+        const message = await mail.sendMail({
+            from: {
+                name: 'Equipe Plann.er',
+                address: 'pedro@planner.com' 
+            },
+            to:{
+                name: owner_email,
+                address: owner_email
+            },
+            subject: 'testando envio de e-mail',
+            html: '<p>teste do envio de email do planner</p>'
+        })
+
+        console.log(nodemailer.getTestMessageUrl(message))
+        return {tripID: trip.id}
     })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import fastify from "fastify";
 import { createTrip } from "./routes/create-trips";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 
 const app = fastify()
+
+// Add schema validator and serializer
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip)
 
