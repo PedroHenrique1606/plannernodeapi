7ba146f0699068b136ee4b8b48019ad67f6c8c19
PedroHenrique1606 <pedromelo.dev.contato@gmail.com>
2024-07-10 23:17:36 -0300

    feat: implemented body email and others participants

--- src/routes/create-trips.ts
+++ src/routes/create-trips.ts
@@ -1,56 +1,89 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
-import {z} from 'zod'
+import { z } from 'zod'
 import dayjs from "dayjs";
 import { prisma } from "../lib/prisma";
 import nodemailer from 'nodemailer'
 import { getMailClient } from "../lib/mail";
 
-export async function createTrip(app:FastifyInstance){
-    app.withTypeProvider<ZodTypeProvider>().post('/trips',{
-        schema:{
+export async function createTrip(app: FastifyInstance) {
+    app.withTypeProvider<ZodTypeProvider>().post('/trips', {
+        schema: {
             body: z.object({
                 destination: z.string().min(4),
                 starts_at: z.coerce.date(),
                 ends_at: z.coerce.date(),
                 owner_name: z.string(),
                 owner_email: z.string().email(),
+                emails_to_invite: z.array(z.string().email())
             })
         }
     }, async (request) => {
-        const {destination,ends_at, starts_at, owner_email, owner_name} = request.body
+        const { destination, ends_at, starts_at, owner_email, owner_name, emails_to_invite } = request.body
 
-        if(dayjs(starts_at).isBefore(new Date())){
+        if (dayjs(starts_at).isBefore(new Date())) {
             throw new Error('Invalid trip start date')
         }
 
-        if(dayjs(ends_at).isBefore(starts_at)){
+        if (dayjs(ends_at).isBefore(starts_at)) {
             throw new Error('Invalid trip end date')
         }
 
-        const trip  = await prisma.trip.create({
+
+        const trip = await prisma.trip.create({
             data: {
                 destination,
                 starts_at,
-                ends_at
+                ends_at,
+                participants: {
+                    createMany: {
+                        data: [
+                            {
+                                name: owner_name,
+                                email: owner_email,
+                                is_owner: true,
+                                is_confirmed: true,
+                            },
+                            ...emails_to_invite.map(email => {
+                                return { email }
+                            })
+                        ],
+                    }
+                }
             }
         })
+
         const mail = await getMailClient()
 
         const message = await mail.sendMail({
             from: {
                 name: 'Equipe Plann.er',
-                address: 'pedro@planner.com' 
+                address: 'pedro@planner.com'
             },
-            to:{
+            to: {
                 name: owner_email,
                 address: owner_email
             },
-            subject: 'testando envio de e-mail',
-            html: '<p>teste do envio de email do planner</p>'
+            subject: `Confirme sua viagem para ${destination}` ,
+            html: `
+                <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+                    <p>Você solicitou a criação de uma viagem para <strong>${destination}</strong> nas datas de
+                        <strong>16 a 27 de Outubro de
+                            2024.</strong>
+                    </p>
+                    <p>
+                    <p></p>
+                    <a href="">
+                        Confirmar Viagem
+                    </a>
+                    </p>
+                    <p></p>
+                    <p>Caso você não saiba do que se trata esse e-mail, apenas o ignore.</p>
+                </div>￼
+            `.trim()
         })
 
         console.log(nodemailer.getTestMessageUrl(message))
-        return {tripID: trip.id}
+        return { tripID: trip.id }
     })
 }
