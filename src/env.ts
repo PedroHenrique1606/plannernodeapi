3bb9ecb8ebff9fbda08597ba6002cf25b75c1b4b
PedroHenrique1606 <pedromelosilva11911@gmail.com>
2024-08-29 19:59:03 -0300

    change port

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,7 @@ const envsSchema = z.object({
     DATABASE_URL: z.string().url(),
     WEB_BASE_URL: z.string().url(),
     API_BASE_URL: z.string().url(),
-    PORT: z.coerce.number().default(3333),
+    PORT: z.coerce.number().default(4000),
     USER_EMAIL: z.string(),
     PASSWORD_MAIL: z.string(),
 })
