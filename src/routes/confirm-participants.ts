2ba6393538f18c8a48fd2e7bd96cc3676d983055
PedroHenrique1606 <pedromelosilva11911@gmail.com>
2024-09-08 17:10:15 -0300

    feat: ajusment confirm participant

--- src/routes/confirm-participants.ts
+++ src/routes/confirm-participants.ts
@@ -7,34 +7,52 @@ import { ClientError } from "../erros/client-error";
 import { env } from "../env";
 
 export async function confirmParticipants(app: FastifyInstance) {
-    app.withTypeProvider<ZodTypeProvider>().get('/participants/:participantId/confirm', {
+    app.withTypeProvider<ZodTypeProvider>().patch('/participants/:participantId/confirm', {
         schema: {
             params: z.object({
                 participantId: z.string().uuid(),
-            })
+            }),
+            body: z.object({
+                email: z.string().email(),
+                name: z.string().min(1),
+            }),
         }
     }, async (request, reply) => {
-        const { participantId } = request.params
+        const { participantId } = request.params;
+        const { email, name } = request.body;
 
+        // Procura o participante pelo participantId
         const participant = await prisma.participant.findUnique({
             where: {
                 id: participantId,
             }
-        })
-
+        });
         if (!participant) {
-            throw new ClientError('Participant not found.')
+            throw new ClientError('Participant not found.');
         }
 
-        if (participant.is_confirmed) {
-            return reply.redirect(`${env.WEB_BASE_URL}/trips/${participant.trip_id}`)
+        // Verifica se o participante existe
+
+        // Verifica se o email fornecido corresponde ao email do participante
+        if (participant.email !== email) {
+            throw new ClientError('Email does not match.');
         }
 
+        // Verifica se o participante já confirmou a presença
+        // if (participant.is_confirmed) {
+        //     return reply.redirect(`${env.WEB_BASE_URL}/trips/${participant.trip_id}`);
+        // }
+
+        // Atualiza o nome e confirma a presença
         await prisma.participant.update({
             where: { id: participantId },
-            data: { is_confirmed: true }
-        })
+            data: {
+                is_confirmed: true,
+                name: name,
+            }
+        });
 
-        return reply.redirect(`${env.WEB_BASE_URL}/trips/${participant.trip_id}`)
-    })
+        return reply.status(204).send()
+        // reply.redirect(`${env.WEB_BASE_URL}/trips/${participant.trip_id}`);
+    });
 }
